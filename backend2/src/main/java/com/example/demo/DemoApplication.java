02d030d11991aaca81ec24a01340ee2b376fd9d1
Deshan.Official <deshan.w@magises.com>
2025-09-06 14:12:19 +0530

    Add Index and Name

--- backend2/src/main/java/com/example/demo/DemoApplication.java
+++ backend2/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class DemoApplication {
 
+    //GS/COMP/325
+    //DESHAN VIMUKTHI
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello from Java Backend!";
